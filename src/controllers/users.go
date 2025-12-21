5a5c100a518ee2d8a30d9de11da68789ffba1dea
Rafael Gomes Souza <rafaelgomes1404@gmail.com>
2025-12-21 14:57:45 -0300

    feat(get-by-id): adding the user get by id to user controller

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -49,9 +49,29 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 
 // GetUserByID gets the user inside database
 func GetUserByID(w http.ResponseWriter, r *http.Request) {
-	if _, error := w.Write([]byte("get by id")); error != nil {
-		log.Fatal(error)
+	parameters := mux.Vars(r)
+
+	userID, error := strconv.ParseUint(parameters["userID"], 10, 64)
+	if error != nil {
+		responses.Error(w, http.StatusBadRequest, error)
+		return
 	}
+
+	db, error := database.Connect()
+	if error != nil {
+		responses.Error(w, http.StatusInternalServerError, error)
+		return
+	}
+	defer closeDB(db)
+
+	repository := repositories.NewUserRepository(db)
+	user, error := repository.FindByID(userID)
+	if error != nil {
+		responses.Error(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, user)
 }
 
 // CreateUser inserts the user inside database
